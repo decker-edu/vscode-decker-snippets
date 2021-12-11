3b030dcd04bbadf50115eef57c518b2e04251595
Sebastian Lukas Hauer <sebastian.hauer@udo.edu>
2021-12-11 02:52:17 +0100

    added multiple more snippets and commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,3 @@
-import { ChildProcess, ChildProcessWithoutNullStreams, exec, spawn } from 'child_process';
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
@@ -13,13 +12,15 @@ export function activate(context: vscode.ExtensionContext) {
 	registerInsert(context, "decker-snippets.inlinelatex", "\$${1:LaTeX}\$");
 	registerInsert(context, "decker-snippets.displaylatex", "\$\$${1:LaTeX}\$\$");
 	registerInsert(context, "decker-snippets.incrementaldisplaylatex", "[\$\$\n\\begin{eqnarry*}\n${1:First Step} \\\\ \n${2:Second Step} \\\\ \n\\end{eqnarry*}\n\$\$]{ .math-incremental }");
+	registerInsert(context, "decker-snippets.code", "```\n${1:Code}\n```");
 
 	registerChange(context, "decker-snippets.make-bold", "**${TM_SELECTED_TEXT:${1:Text}}**");
 	registerChange(context, "decker-snippets.make-italic", "*${TM_SELECTED_TEXT:${1:Text}}*");
 	registerChange(context, "decker-snippets.make-image", "![${1:alt-text}](${TM_SELECTED_TEXT:${2:Link}})");
-	registerChange(context, "decker-snippets.make-link", "[${1:Text}](${TM_SELECTED_TEXT:${2:Text}} \"{3:Hover Text}\")");
+	registerChange(context, "decker-snippets.make-link", "[${1:Text}](${TM_SELECTED_TEXT:${2:Text}} \"${3:Hover Text}\")");
 	registerChange(context, "decker-snippets.make-quote", "> ${TM_SELECTED_TEXT:${1:Text}}");
 	registerChange(context, "decker-snippets.make-latex", "\$${TM_SELECTED_TEXT:${1:Text}}\$");
+	registerChange(context, "decker-snippets.make-code", "```\n${TM_SELECTED_TEXT:${1:Code}}\n```");
 }
 
 function registerInsert(context: vscode.ExtensionContext, command: string, snippet: string) {
